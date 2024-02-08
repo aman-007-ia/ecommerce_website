b872e4b8a8461181db0d64c9dbab617f7065fc31
aman-007-ia <amanyadav794172@gmail.com>
2024-02-08 14:35:27 +0530

    create first product page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Counter } from "./features/counter/Counter";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <Home></Home>
     </div>
   );
 }
