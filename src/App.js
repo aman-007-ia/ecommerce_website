ab136a9851e4cfba5fd6d5d8c2d2ddf689f2a487
aman-007-ia <amanyadav794172@gmail.com>
2024-02-09 17:43:45 +0530

    cart page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    //only for testing - then page will be added
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 
